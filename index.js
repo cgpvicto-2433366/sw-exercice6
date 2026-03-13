1c245e4de52cb806ad05fa40d5f18ad177257473
Ben Schalom KAMGA <benschalom8@gmail.com>
2026-03-13 14:05:04 -0400

    Correction de index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import router from './src/routes/pockemon.route.js';
 import swaggerUi from 'swagger-ui-express';
 import * as OpenApiValidator from 'express-openapi-validator';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const swaggerDocument = JSON.parse(
     fs.readFileSync('./src/config/documentation.json', 'utf8')
